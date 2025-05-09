c63cb8929f4c894b2da716cabfd1b504ae4c61d2
Rohit gorai <rg7558674@gmail.com>
2025-05-09 22:43:27 +0530

    learned somr control flow statements

---
+++ script.js
@@ -0,0 +1,36 @@
+
+// contrl flow 
+// if 
+
+
+// // odd even 
+// let num = prompt("Enter a number:");
+// num = Number(num);
+
+// if (num % 2 === 0) {
+//     console.log("Even number");
+// } else {
+//     console.log("Odd number");
+// }
+
+// //positive ,negative , zero 
+// let num = Number(prompt("Enter a number:"));
+
+// if (num > 0) {
+//     console.log("Positive");
+// } else if (num < 0) {
+//     console.log("Negative");
+// } else {
+//     console.log("Zero");
+// }
+
+// vowel or consonant
+// let ch = prompt("Enter a character:").toLowerCase();
+
+// if ("aeiou".includes(ch)) {
+//     console.log("Vowel");
+// } else if (ch >= 'a' && ch <= 'z') {
+//     console.log("Consonant");
+// } else {
+//     console.log("Not an alphabet");
+// }
