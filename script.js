7ac6ab64a1dc5424f7ae2a589ca522f6966bcbeb
Rohit gorai <rg7558674@gmail.com>
2025-05-10 14:44:01 +0530

    masterd at loops

--- script.js
+++ script.js
@@ -40,9 +40,37 @@
 // 1 for loop
 // basic syntax 
 
-for (let i  = 0; i  < 10 ; i ++) {
-    const element = array[i ];
+// for (let i  = 0; i  < 10 ; i ++) {
+//     const element = array[i ];
 
-}
+// }
 
 // mostly used for arrays and its operations 
+
+
+// while loop and do while loop 
+
+// initialization is done globaly or outside the scope  then the condition is checked increment or decrement also needed to terminste the program 
+ 
+// let a= 0
+
+// while (a<=10) {
+ 
+//     console.log('hello rohit ');
+//     a+=1
+// }
+
+
+// do while loop 
+
+// initializatiobn is done globaly or outside the scope , then do executes the code whatever is written on its scope ,, thereafter the condition is checked 
+
+// increment or decrement is also important to terminate the loop 
+ 
+// let  b = 22
+
+
+// do {
+//     console.log('hello rohit ');
+//     b-=3
+// } while (b>=1);
