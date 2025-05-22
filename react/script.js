33021e1a458aea1f954befdc40ad7f5b4e16f7ee
Rohit gorai <rg7558674@gmail.com>
2025-05-22 12:40:03 +0530

    added new files

---
+++ react/script.js
@@ -0,0 +1,73 @@
+
+    // const heading = React.createElement("h1", {id : "heading "} , "hello rohit from react !") // the {}is for giving attributes to the html elements like :: id , class , etc..
+     
+    
+    // <div id= "parent">
+              
+    //     <div id="child">
+    //       <h2> hello rohit by react </h2>  // how to create nested html elements in react js 
+    //     </div>
+    // </div>
+    
+
+
+    // const parent = React.createElement("div",
+    //      { id :"parent"},
+    //       React.createElement("div",
+    //          { id :"child"}, 
+    //         React.createElement("h1", {},"hello rohit by javascript" ) ) )
+    
+    
+    // const root = ReactDOM.createRoot(document.getElementById("root"))
+
+    //    root.render(parent)
+
+
+
+       // how to create siblings in html using react ?/
+       // like this 
+
+
+        
+    // <div id= "parent">
+              
+    //     <div id="child">
+    //       <h2> hello rohit by react </h2>  
+    //       <h2> hello rohit by react </h2>  
+    //       <h2> hello rohit by react </h2>  
+    //     </div>
+    // </div>
+    
+
+    // we have to pass the third argument as array 
+
+    const parent = React.createElement("div",
+         { id :"parent"},
+         [ React.createElement("div",
+             { id :"child"}, 
+           [ React.createElement("h1", {},"hello rohit by javascript" ) 
+            ,React.createElement("h1", {},"hello rohit by javascript" )] // here we can see that the third element is passed as array 
+           
+         ),
+       React.createElement("div",
+             { id :"child"}, 
+           [ React.createElement("h1", {},"hello rohit by javascript" ) 
+            ,React.createElement("h1", {},"hello rohit by javascript" )] // here we can see that the third element is passed as array 
+           
+         )] )
+    
+    
+    const root = ReactDOM.createRoot(document.getElementById("root")) 
+    
+    const rohit = React.createElement("div" ,{ id :""} , "")
+
+//     <div>
+//   Hello Rohit bhai!
+//   <button>Click Me</button>
+// </div>
+
+
+  
+
+
+       root.render(parent)
