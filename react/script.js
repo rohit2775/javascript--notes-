512d315d0bc70a5daaf455b8c0b08dfd3b5b2a3d
Rohit gorai <rg7558674@gmail.com>
2025-05-24 10:43:44 +0530

    learning from an old video , getting too many errors

--- react/script.js
+++ react/script.js
@@ -1,90 +1,18 @@
 
-    // const heading = React.createElement("h1", {id : "heading "} , "hello rohit from react !") // the {}is for giving attributes to the html elements like :: id , class , etc..
-     
-    
-    // <div id= "parent">
-              
-    //     <div id="child">
-    //       <h2> hello rohit by react </h2>  // how to create nested html elements in react js 
-    //     </div>
-    // </div>
-    
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 
-    // const parent = React.createElement("div",
-    //      { id :"parent"},
-    //       React.createElement("div",
-    //          { id :"child"}, 
-    //         React.createElement("h1", {},"hello rohit by javascript" ) ) )
+    const parent = React.createElement("div",
+         { id :"parent"},
+         React.createElement("div",
+             { id :"child"}, 
+            [React.createElement("h1", {key : 1},"hello rohit by javascript" ) ,React.createElement("h1", {key : 2},"hello rohit by javascript" )]
+           ) )
     
     
-    // const root = ReactDOM.createRoot(document.getElementById("root"))
-
-    //    root.render(parent)
-
-
-
-       // how to create siblings in html using react ?/
-       // like this 
-
-
-        
-    // <div id= "parent">
-              
-    //     <div id="child">
-    //       <h2> hello rohit by react </h2>  
-    //       <h2> hello rohit by react </h2>  
-    //       <h2> hello rohit by react </h2>  
-    //     </div>
-    // </div>
-    
-
-    // we have to pass the third argument as array 
-
-
-
- // TO create a html like this in react 
- //<div id= "parent">
-              
-    //     <div id="child">
-    //       <h2> hello rohit by react </h2>  
-    //       <h2> hello rohit by react </h2>  
-    //       <h2> hello rohit by react </h2>  
-    //     </div>
-         // <div id="child">
-    //       <h2> hello rohit by react </h2>  
-    //       <h2> hello rohit by react </h2>  
-    //       <h2> hello rohit by react </h2>  
-    //     </div>
-    // </div>
- 
-
-
-   //  const parent = React.createElement("div",
-   //       { id :"parent"},
-   //       [ React.createElement("div",
-   //           { id :"child"}, 
-   //         [ React.createElement("h1", {},"hello rohit by javascript" ) 
-   //          ,React.createElement("h1", {},"hello rohit by javascript" )] // here we can see that the third element is passed as array 
-           
-   //       ),
-   //     React.createElement("div",
-   //           { id :"child"}, 
-   //         [ React.createElement("h1", {},"hello rohit by javascript" ) 
-   //          ,React.createElement("h1", {},"hello rohit by javascript" )] // here we can see that the third element is passed as array 
-           
-   //       )] )
-    
-    
-   //  const root = ReactDOM.createRoot(document.getElementById("root")) 
-   //   root.render(parent) // . render use - uss selected div me kuch bhi hoga usko replace krega passed arguments se 
-
-    // what is npm ??
-    // will learn in our react notes /  / for react notes just go through my repository named- react-notes
-    
-     
-
-
+    const root = ReactDOM.createRoot(document.getElementById("root")) 
+     root.render(parent) // 
   
 
 
