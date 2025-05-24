357e5f5cdac0b680c530412be92d878cd94e99a4
Rohit gorai <rg7558674@gmail.com>
2025-05-24 22:11:58 +0530

    getting some errrors , changes not reflecting

--- react/script.js
+++ react/script.js
@@ -1,19 +1,15 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, " hello rohit bhai kaise ho")
+  )
+);
 
-    const parent = React.createElement("div",
-         { id :"parent"},
-         React.createElement("div",
-             { id :"child"}, 
-            [React.createElement("h1", {key : 1},"hello rohit by javascript" ) ,React.createElement("h1", {key : 2},"hello rohit by javascript" )]
-           ) )
-    
-    
-    const root = ReactDOM.createRoot(document.getElementById("root")) 
-     root.render(parent) // 
-  
-// 
-
-      
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
