0ea61b3f26517b2c2f01a4d8bfdeb4728f5664f2
rohit2775 <rg7558674@gmail.com>
2025-12-31 23:32:17 +0530

    prototype begins

--- PROMISEs/prototype.js
+++ PROMISEs/prototype.js
@@ -6,7 +6,12 @@
  createUser.prototype.increment = function(){
    this.price ++
  }
+ createUser.prototype.printme = function(){
+    console.log(`the price of ${this.user}  is  ${this.price}`);
+ }
   // yaha pr increment ko pata nai chaega ki kis value ko increment karna hai to yaha prr hamlog this keyword ka use karenge 
 
- const chai = createUser("chai", 30)
- const tea = createUser("tea", 250)
+ const chai = new createUser("chai", 30)
+ const tea = new createUser("tea", 250)
+
+ chai.printme()
