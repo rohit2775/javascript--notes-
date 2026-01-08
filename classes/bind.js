7c3e720fc4d07f60374ae9e2b5d165fb5941b158
rohit2775 <rg7558674@gmail.com>
2026-01-08 20:37:56 +0530

    example

--- classes/bind.js
+++ classes/bind.js
@@ -12,3 +12,15 @@ greetFn();
 // bind() is a JavaScript method that creates a new function with a fixed this value, bound permanently to a specific object.bind() ek naya function banata hai aur us function ka this permanently kisi ek object ke saath jod deta hai, chahe function kahin se bhi call ho.
 
 // bind() ek naya function banata hai aur us function ka this permanently kisi ek object ke saath jod deta hai, chahe function kahin se bhi call ho.
+
+function sayName() {
+  console.log(this.name);
+}
+
+const User = { name: "Rohit" };
+
+const fixed = sayName.bind(user);
+fixed(); // Rohit
+
+
+//one more example 
