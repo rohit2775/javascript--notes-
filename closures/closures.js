7981d02fd102e6838839661305595bb52b82ddff
rohit2775 <rg7558674@gmail.com>
2026-01-10 20:23:42 +0530

    knowing the meaning of closures and lexical scopes  with examples

--- closures/closures.js
+++ closures/closures.js
@@ -1,1 +1,21 @@
 //       lexical scopes - A function can access variables that are defined in the place where the function is written.
+
+function bankAccount() {
+  let balance = 1000;
+
+  return {
+    deposit(amount) {
+      balance += amount;
+      console.log(balance);
+    },
+    withdraw(amount) {
+      balance -= amount;
+      console.log(balance);
+    }
+  };
+}
+
+const account = bankAccount();
+account.deposit(500) 
+
+
