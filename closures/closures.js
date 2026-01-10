eca90b04c48dab09aa256e17aa4d3968c1c58e56
rohit2775 <rg7558674@gmail.com>
2026-01-10 20:28:23 +0530

    knowing the meaning of closures and lexical scopes  with examples

--- closures/closures.js
+++ closures/closures.js
@@ -1,5 +1,5 @@
 //       lexical scopes - A function can access variables that are defined in the place where the function is written.
-
+ // A closure is created when a function remembers variables from its lexical scope, even after the outer function has finished executing. 
 function bankAccount() {
   let balance = 1000;
 
@@ -19,3 +19,5 @@ const account = bankAccount();
 account.deposit(500) 
 
 
+// Lexical Scope = access kaha se milega
+// Closure = yaad rakhna after execution
